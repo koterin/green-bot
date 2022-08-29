c061fa143936d1e4ad266b8c856862e387933fef
koterin <katerine.grin@gmail.com>
2022-08-29 17:44:36 +0300

    fixes for refactoring

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,15 @@
-FROM golang:1.18
-COPY ./ /green-bot
+FROM golang:1.18 AS builder
+WORKDIR /build
+
+COPY . /build
+
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -ldflags '-extldflags "-static"' -o main main.go
+
+FROM alpine:3.16 as server
+RUN apk --no-cache add ca-certificates
+
+COPY --from=builder /build/main /green-bot/
+
 WORKDIR /green-bot
 
-RUN go build -o green-bot
-ENTRYPOINT ["./green-bot"]
+ENTRYPOINT [ "./main" ]
--- utils/utils.go
+++ utils/utils.go
@@ -1,58 +1,59 @@
 package utils
 
 import (
-    "context"
-    "log"
-    "strconv"
-    "time"
+	"context"
+	"strconv"
+	"time"
 
-    telebot "gopkg.in/tucnak/telebot.v2"
+	log "github.com/sirupsen/logrus"
+
+	telebot "gopkg.in/tucnak/telebot.v2"
 )
 
 type Recipient struct {
-    ID int
+	ID int
 }
 
 func (user Recipient) Recipient() string {
-    return strconv.Itoa(user.ID)
+	return strconv.Itoa(user.ID)
 }
 
 func StartTelegramBot(ctx context.Context, TG_BOT_KEY string) {
-    settings := telebot.Settings{
-        Token: TG_BOT_KEY,
-        Poller: &telebot.LongPoller{
-            Timeout: 1 * time.Second,
-        },
-    }
+	settings := telebot.Settings{
+		Token: TG_BOT_KEY,
+		Poller: &telebot.LongPoller{
+			Timeout: 1 * time.Second,
+		},
+	}
 
-    bot, err := telebot.NewBot(settings)
-    if err != nil {
-        log.Fatal(err)
-    }
+	bot, err := telebot.NewBot(settings)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    bot.Handle("/start", func(m *telebot.Message) {
-        if !m.Private() {
-            log.Println("Error: chat is not private")
-            return
-        }
+	bot.Handle("/start", func(m *telebot.Message) {
+		if !m.Private() {
+			log.Error("Error: chat is not private")
+			return
+		}
 
-        log.Println("User started bot: ", m.Sender.Username)
+		log.Info("User started bot: ", m.Sender.Username)
 
-        var userChat Recipient
-        userChat.ID = int(m.Chat.ID)
+		var userChat Recipient
+		userChat.ID = int(m.Chat.ID)
 
-        message := "Сообщи этот ID админу для авторизации: " + userChat.Recipient()
-        bot.Send(userChat, message)
-    })
+		message := "Сообщи этот ID админу для авторизации: " + userChat.Recipient()
+		bot.Send(userChat, message)
+	})
 
-    go func() {
-        bot.Start()
-    }()
+	go func() {
+		bot.Start()
+	}()
 
-    log.Println("Telegram Bot started")
+	log.Info("Telegram Bot started")
 
-    <-ctx.Done()
+	<-ctx.Done()
 
-    log.Println("Telegram Bot stopped")
-    bot.Stop()
+	log.Info("Telegram Bot stopped")
+	bot.Stop()
 }
